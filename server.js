30b597900100e7103cc2829695a2eac8b691fbc2
karthike-05 <89458592+karthike-05@users.noreply.github.com>
2023-04-02 00:11:12 -0400

    Update server.js

--- server.js
+++ server.js
@@ -13,21 +13,23 @@ const port = arguement.port || 3000;
 // Use the documentation for the Node.js `fs` module. 
 // The function must read a file located at `./public/index.html` and do some stuff with it.
 // The stuff that should be inside this function is all below.
-
 // If there is an error, put it on the console error and return. 
 // Do not be nice about exiting.
 
-
-
-
-
-// Define a const `server` as an arrow function using http.createServer. 
+fs.readFile('./public/index.html',(err,data) => {
+  if(err){
+      console.error(err);
+      return;
+  }
+  
+ // Define a const `server` as an arrow function using http.createServer. 
 // Use the documentation for the node.js http module. 
 // The function should have three responses: 
 // 1. status code 200, 
 // 2. set a header with content type `text/html`, and 
 // 3. end with the data that you are reading in from ./public/index.html.
 
+  const server = http.createServer((
 
 
 
